952c1152a41d6306b747deefc298d2b7d0ba7b4b
mrobeidat <y.linux96@gmail.com>
2024-01-04 23:48:52 +0300

    initial setup + health check route

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Health check route
+@app.get('/health')
+def health_check():
+    return {"status": "ok"}
+    
+# Home route
+@app.get('/')
+def home():
+    return {"message": "Hello World!"}
+
+
